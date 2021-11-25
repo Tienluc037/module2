fd0c891ec8ad953faedd3e818e6cc58207f50216
Tienluc037 <nguyentienluc12a3@gmail.com>
2021-11-25 11:56:48 +0700

    hello

--- test/delete.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "data.php";
-$index = $_REQUEST["id"];
-deleteArr($index);
-header("location:findMin.php");
--- test/test2/data1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$arr = array(
-    array( "name" => "a", "age" => 11, "address" => "dn"),
-    array( "name" => "b", "age" => 10, "address" => "dn"),
-    array( "name" => "c", "age" => 19, "address" => "dn"),
-    array( "name" => "d", "age" => 14, "address" => "dn"),
-);
